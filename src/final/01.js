d38b976adbfd346fa2df79bf734ffdfcc904ed99
Samuel Comeau <24446727+sam-com@users.noreply.github.com>
2024-01-16 15:54:25 -0500

    New: Classcraft React exercises 1-2

--- src/final/01.js
+++ src/final/01.js
@@ -1,18 +1,6 @@
-// useReducer: simple Counter
+// React: Component-Ception 🌀
 // http://localhost:3000/isolated/final/01.js
 
-import * as React from 'react'
-
-const countReducer = (state, newState) => newState
-
-function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+export default function App() {
+  return <div></div>
 }
-
-function App() {
-  return <Counter />
-}
-
-export default App
